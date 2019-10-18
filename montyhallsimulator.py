9d4e2071ff89d44f75e73fecc0ee55bc1a2aa791
Matthew Mercuri <mercurimatthew@gmail.com>
2019-10-18 01:43:20 -0400

    Did some cleaning with flake8

--- montyhallsimulator.py
+++ montyhallsimulator.py
@@ -12,21 +12,21 @@ def play_sim(games, my_choice, total_doors=3):
 	wins = 0
 	losses = 0
 
-	while i < games: 
+	while i < games:
 		game = Game(total_doors)
 		game.make_choice(my_choice)
 		game.montys_choice()
 		game.game_strategy('switch')
-		if game.win == True:
+		if game.win is True:
 			wins += 1
-		elif game.win == False:
+		elif game.win is False:
 			losses += 1
 
 		i += 1
 
 	print('You have', wins, 'wins')
 	print('You have', losses, 'losses')
-	print('This yields a win percentage of:', round((wins / games)*100, 2), '%')
+	print('This yields a win percentage of:', round((wins / games) * 100, 2), '%')
 
 
 def _check_for_win(my_door):
@@ -36,20 +36,20 @@ def _check_for_win(my_door):
 		result = False
 	else:
 		ValueError('No proper value for our chosen door!')
-	
+
 	return result
 
 
 def _create_prize_array(doors):
 	prize_array = np.zeros(doors, dtype=int)
-	random_door = randint(0, doors-1)
-	prize_array[random_door]=1
+	random_door = randint(0, doors - 1)
+	prize_array[random_door] = 1
 
 	return prize_array
 
 
 def _build_simulator(doors):
-	door_array = np.arange(1, doors+1, 1)
+	door_array = np.arange(1, doors + 1, 1)
 	prize_array = _create_prize_array(doors)
 	game_df = pd.DataFrame(data=prize_array, columns=['Prize'], index=door_array)
 
@@ -67,7 +67,7 @@ class Game:
 			print(doors, 'is not an integer! Please pass and int value.')
 
 	def make_choice(self, chosen_door):
-		self.my_door = self.game_df.iloc[[chosen_door-1]]
+		self.my_door = self.game_df.iloc[[chosen_door - 1]]
 		self.my_door = self.my_door['Prize'].iloc[0]
 		self.game_df = self.game_df.drop([chosen_door])
 
@@ -78,7 +78,7 @@ class Game:
 				self.montys_door = self.game_df.iloc[[montys_door]]
 				self.montys_door = self.montys_door['Prize'].iloc[0]
 			else:
-				montys_door = randint(0, self.doors-2)
+				montys_door = randint(0, self.doors - 2)
 				self.montys_door = self.game_df.iloc[[montys_door]]
 				self.montys_door = self.montys_door['Prize'].iloc[0]
 		else:
@@ -94,25 +94,7 @@ class Game:
 		else:
 			raise ValueError("Please pass 'stay' or 'switch'")
 
-		if result == True:
+		if result is True:
 			self.win = True
 		else:
 			self.win = False
-
-
-
-
-
-
-
-
-		
-
-
-#_build_simulator('Hi')
-#_create_prize_array(5)
-#game = Game('Hi')
-#game = Game(5)
-#game.make_choice(2)
-#game.montys_choice()
-#print(game.door_to_leave)
